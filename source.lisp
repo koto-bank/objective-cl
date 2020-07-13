5829c2ce9161cadadc3c94280fc74dd9f5d511a6
alex-eg <alexey.e.egorov@gmail.com>
2020-07-13 19:11:47 +0300

    initial

---
+++ source.lisp
@@ -0,0 +1,39 @@
+(defun make-slot-access (symbol)
+  (unless (symbolp symbol)
+    (return-from make-slot-access symbol))
+  (let* ((str (string symbol))
+         (parsing-obj t)
+         (obj "")
+         (start 0)
+         (slots (list)))
+    (loop
+      :for c :across str
+      :for n :from 0
+      :do (when (char= c #\.)
+            (if parsing-obj
+              (setf parsing-obj nil
+                    obj (subseq str 0 n)
+                    start (1+ n))
+              (progn
+                (push (subseq str start n) slots)
+                (setf start (1+ n))))))
+    (if parsing-obj symbol
+      (progn (push (subseq str start) slots)
+             (reduce
+              (lambda (acc elem)
+                `(slot-value ,acc ',elem))
+              (reverse (mapcar 'intern slots))
+              :initial-value (intern obj))))))
+
+(defun br-reader (stream char)
+  (declare (ignore char))
+  (let ((*readtable* (copy-readtable)))
+    (set-macro-character #\] (lambda (stream char)
+                               (declare (ignore stream char))))
+    (let ((form (loop
+                  :for form := (read steam nil nil t)
+                  :while form :collect form)))
+      (cond ((null form) nil)
+            ((= 1 (length form)) (make-slot-access (car form)))
+            (t (mapcar #'make-slot-access
+                       `(,(second form) ,(first form) ,@(cddr form))))))))
